db65ae99f020e7d20fdb86dff15e9cb84a4b429e
Vladimir Kovalev <vladimir.kovalev@ocrv.ru>
2021-05-28 17:16:59 +0300

    * SQL script changed * db schema added

--- sqlite.py
+++ sqlite.py
@@ -1,11 +1,26 @@
 import sqlite3
+import json
+
+from collections import Counter
 
 conn = sqlite3.connect('db.sqlite')
-cursor = conn.cursor()
 
-sql = "SELECT * FROM movie_actors"
-cursor.execute(sql)
-actors = cursor.fetchall()
+counter = Counter()
+for row in conn.execute('select writer, writers from movies'):
+    writer = row[0]
+    if writer:
+        counter[writer] += 1
+    else:
+        writers = json.loads(row[1])
+        for w in writers:
+            counter[w['id']] += 1
 
-for actor in actors:
-    print(actor)
+writers_ids = [writer_id for writer_id, _ in counter.most_common()[:2]]
+in_placeholders = ','.join(['?'] * len(writers_ids))
+
+row = conn.execute(
+    f"select name from writers where id in ({in_placeholders}) and name != 'N/A' limit 1",
+    writers_ids,
+).fetchone()
+
+print('Most productive writer:', row)
